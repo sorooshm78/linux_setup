2a5ae15c3ba29be458b9d385497a5a12bc88c726
sorooshm78 <sorooshm78@gmail.com>
2022-08-16 20:16:28 +0430

    add script install app

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Enter your app install
+declare -a myApp=(
+	"git"
+	"vim"
+	"tmux"
+)
+
+# color
+red="\033[1;31m"
+green="\033[1;32m"
+yellow="\033[1;33m"
+blue="\033[1;34m"
+purple="\033[1;35m"
+cyan="\033[1;36m"
+grey="\033[0;37m"
+reset="\033[m"
+
+# Update ripository
+echo -e "${green}*** update repository ***${reset}"
+sudo apt update -y
+echo -e "${green}*** finish update ***${reset}"
+
+# Inistall app
+for app in ${myApp[@]}
+do
+	echo -e "${red}*** install $app ***${reset}"
+	sudo apt install -y  $app
+	echo -e "${red}*** finish install $app ***${reset}\n"
+done
